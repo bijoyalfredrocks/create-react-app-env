581a92f48bd511b63b9e0169563211fe554c85ff
bijoyalfredrocks <bijoyalfred1983@gmail.com>
2018-10-07 06:45:49 +0530

    Updates

---
+++ precache-manifest.94911e75cc6cc1a77e1a1f12b77b03a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b30cf44da39dfa2f2444",
+    "url": "/create-react-app-env/static/js/runtime~main.b30cf44d.js"
+  },
+  {
+    "revision": "b40be0e582c3263940e5",
+    "url": "/create-react-app-env/static/js/main.b40be0e5.chunk.js"
+  },
+  {
+    "revision": "0fa571ce64f5bf23ea8d",
+    "url": "/create-react-app-env/static/js/1.0fa571ce.chunk.js"
+  },
+  {
+    "revision": "b40be0e582c3263940e5",
+    "url": "/create-react-app-env/static/css/main.5b523cf9.chunk.css"
+  },
+  {
+    "revision": "dca64807209b5a8e7ddb32dbf84ffe1c",
+    "url": "/create-react-app-env/index.html"
+  }
+];
